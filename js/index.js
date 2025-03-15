86fa6ef47c9ced2dcaa92917fda05c2b162515de
sarwar0001 <140310771+imzaidsarwar@users.noreply.github.com>
2025-03-15 16:45:41 +0530

    remove extra code

--- js/index.js
+++ js/index.js
@@ -7,28 +7,28 @@ function toggleMenu() {
   // Toggle Dark Mode
   const modeSwitch = document.getElementById('dynamic-text');
   
-  modeSwitch.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode');
+  // modeSwitch.addEventListener('change', () => {
+  //   document.body.classList.toggle('dark-mode');
     
-    // Save mode to localStorage
-    if (document.body.classList.contains('dark-mode')) {
-      localStorage.setItem('theme', 'dark');
-    } else {
-      localStorage.setItem('theme', 'light');
-    }
-  });
+  //   // Save mode to localStorage
+  //   if (document.body.classList.contains('dark-mode')) {
+  //     localStorage.setItem('theme', 'dark');
+  //   } else {
+  //     localStorage.setItem('theme', 'light');
+  //   }
+  // });
   
-  // Load saved theme from localStorage (default to light mode)
-  window.onload = () => {
-    const savedTheme = localStorage.getItem('theme') || 'light'; // Default to light mode
-    if (savedTheme === 'dark') {
-      document.body.classList.add('dark-mode');
-      modeSwitch.checked = true;
-    } else {
-      document.body.classList.remove('dark-mode');
-      modeSwitch.checked = false;
-    }
-};
+  // // Load saved theme from localStorage (default to light mode)
+  // window.onload = () => {
+  //   const savedTheme = localStorage.getItem('theme') || 'light'; // Default to light mode
+  //   if (savedTheme === 'dark') {
+  //     document.body.classList.add('dark-mode');
+  //     modeSwitch.checked = true;
+  //   } else {
+  //     document.body.classList.remove('dark-mode');
+  //     modeSwitch.checked = false;
+  //   }
+//};
 
 
 // Typewriter Effect
